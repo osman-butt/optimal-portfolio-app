bbb1e3fd99aed9f898f90a6a1ededec4825466c6
osman-butt <butt.osman@gmail.com>
2024-11-27 22:05:43 +0100

    rename to app.py

--- stock-service/app.py
+++ stock-service/app.py
@@ -45,4 +45,6 @@ def calculate_portfolio_metrics():
 
 # Run the Flask app
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = 5000
+    app.run(debug=False, host="0.0.0.0", port=port)
+    # app.run(debug=True, host="0.0.0.0", port=port)
