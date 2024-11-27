c1c84b5b36bb494db986caa6e5cede34c0f8ba37
osman-butt <butt.osman@gmail.com>
2024-11-27 15:06:00 +0100

    added global exceptionhandler

---
+++ portfolio-service/src/main/java/dev/osmanb/portfolioService/exception/ApiException.java
@@ -0,0 +1,35 @@
+package dev.osmanb.portfolioService.exception;
+
+import org.springframework.http.HttpStatus;
+
+import java.time.ZonedDateTime;
+
+public class ApiException {
+    private final String message;
+    private final HttpStatus error;
+    private final int statusCode;
+    private final ZonedDateTime timeStamp;
+
+    public ApiException(String message, HttpStatus error, ZonedDateTime timeStamp) {
+        this.message = message;
+        this.statusCode = error.value();
+        this.error = error;
+        this.timeStamp = timeStamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public HttpStatus getError() {
+        return error;
+    }
+
+    public ZonedDateTime getTimeStamp() {
+        return timeStamp;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+}
---
+++ portfolio-service/src/main/java/dev/osmanb/portfolioService/exception/UnprocessableContent.java
@@ -0,0 +1,19 @@
+package dev.osmanb.portfolioService.exception;
+
+public class UnprocessableContent extends RuntimeException{
+    public UnprocessableContent() {
+    }
+
+    public UnprocessableContent(String message) {
+        super(message);
+    }
+
+    public UnprocessableContent(Throwable cause) {
+        super(cause);
+    }
+
+    public UnprocessableContent(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
